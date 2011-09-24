7e87169f26edb37322047bc75c4c38b9ae1549b0
Lucas Clemente Vella <lvella@gmail.com>
2011-09-24 19:57:17 -0300

    Another login strategy.

---
+++ diggems/static/login.js
@@ -0,0 +1,15 @@
+function fb_login() {
+    FB.login(function(response) {
+	if (response.authResponse) {
+	    console.log('Welcome!  Fetching your information.... ');
+	    FB.api('/me', function(response) {
+		console.log('Good to see you, ' + response.name + '.');
+		FB.logout(function(response) {
+		    console.log('Logged out.');
+		});
+	    });
+	} else {
+	    console.log('User cancelled login or did not fully authorize.');
+	}
+    });
+}
