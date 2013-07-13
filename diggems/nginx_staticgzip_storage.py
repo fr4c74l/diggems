e2a42c97038ababec2bd5c9d7eae25040d42d74d
Lucas Clemente Vella <lvella@gmail.com>
2013-07-13 11:52:58 -0300

    Working gziping collectstatic.

--- diggems/nginx_staticgzip_storage.py
+++ diggems/nginx_staticgzip_storage.py
@@ -1,50 +1,64 @@
 import os
+import os.path
 import subprocess
 import gzip
+import traceback
 from cStringIO import StringIO
 
 from django.contrib.staticfiles.storage import StaticFilesStorage
+from django.core.files.base import ContentFile
 
 class NginxStaticGZIPStorage(StaticFilesStorage):
     def post_process(self, paths, **options):
         for name in paths:
             gz_name = name + '.gz'
 
-            storage, orig_path = paths[name]
-            orig_path = storage.path(name)
-            gz_path = storage.path(gz_name)
+            try:
+                if self.exists(gz_name):
+                    if self.modified_time(gz_name) >= self.modified_time(name):
+                        continue
 
-            if storage.exists(gz_name):
-                storage.delete(gz_name)
+                orig_path = self.path(name)
+                gz_path = self.path(gz_name)
 
-            # First, gzip with command line tool
-            tmp_path = '/tmp' + name
-            os.link(orig_path, tmp_path)
-            subprocess.call(['gzip', '-9', '-f', tmp_path])
-            os.rename(tmp_path, gz_path)
-            cmd_size = storage.size(gz_name)
+                if self.exists(gz_name):
+                    self.delete(gz_name)
 
-            # Then, gzip in memory
-            zcontents = StringIO()
-            with gzip.GzipFile(gz_name, 'wb', 9, zcontents) as gzfile:
-                gzfile.write(storage.open(name).read())
-            embed_size = len(zcontents.getvalue())
+                # First, gzip with command line tool
+                tmp_path = os.path.basename(name)
+                os.link(orig_path, tmp_path)
+                subprocess.call(['gzip', '-9', '-f', tmp_path])
+                os.rename(tmp_path + '.gz', gz_path)
+                cmd_size = self.size(gz_name)
 
-            # Get original size, for comparision
-            orig_size = storage.size(name)
+                # Then, gzip in memory
+                zcontents = StringIO()
+                gz_filename = os.path.basename(gz_name).encode('utf-8')
+                with gzip.GzipFile(gz_filename, 'wb', 9, zcontents) as gzfile:
+                    gzfile.write(self.open(name, 'rb').read())
+                zcontents = zcontents.getvalue()
+                embed_size = len(zcontents)
 
-            min_size = min(orig_size, cmd_size, embed_size)
-            if min_size == orig_size:
-                storage.delete(gz_name)
-                gzipped = False
-                print 'Using original'
-            elif min_size == cmd_size:
-                gzipped = True
-                print 'Using command line gzip'
-            else:
-                zcontents.seek(0)
-                storage.save(gz_name, zcontents)
-                gzipped = True
-                print "Using python's gzip"
+                # Get original size, for comparision
+                orig_size = self.size(name)
+
+                print 'orig size:', orig_size, 'external gzip size:', cmd_size, 'internal gzip size:', embed_size
+                min_size = min(orig_size, cmd_size, embed_size)
+                if min_size == orig_size:
+                    print 'Using original'
+                    self.delete(gz_name)
+                    gzipped = False
+                elif min_size == cmd_size:
+                    print 'Using command line gzip'
+                    gzipped = True
+                else:
+                    print "Using python's gzip"
+                    self.delete(gz_name)
+                    self.save(gz_name, ContentFile(zcontents))
+                    subprocess.call(['touch', '-r', orig_path, gz_path])
+                    gzipped = True
+            except:
+                traceback.print_exc()
+                raise
 
             yield name, gz_name, gzipped
