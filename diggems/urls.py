7468e033191daa53e737179769da596d38db0fc4
Tuanir FR <tfr011@gmail.com>
2013-07-20 18:31:23 -0300

    Donation translation mark

--- diggems/urls.py
+++ diggems/urls.py
@@ -20,7 +20,7 @@ urlpatterns = patterns (
     (r'^fb/login/', 'game.views.fb_login'),
     (r'^fb/logout/', 'game.views.fb_logout'),
     (r'^info/(?P<page>.*)/', 'game.views.info'),
-
+    (r'^donate/','game.views.donate'),
     (r'^i18n/', include('django.conf.urls.i18n')),
     (r'^jsi18n/$', 'django.views.i18n.javascript_catalog', js_info_dict),
 
--- game/views.py
+++ game/views.py
@@ -382,9 +382,13 @@ def move(request, game_id):
 
     return HttpResponse()
 
+def donate(request):
+    profile = UserProfile.get(request)
+    return render_with_extra('donate.html', profile)
+
 def info(request, page):
     actual_locale = to_locale(get_language())
     if page not in info.existing_pages:
         raise Http404
     return render_with_extra('{}/{}.html'.format(actual_locale, page), UserProfile.get(request))
-info.existing_pages = frozenset(('about', 'howtoplay', 'sourcecode', 'donate'))
+info.existing_pages = frozenset(('about', 'howtoplay', 'sourcecode'))
