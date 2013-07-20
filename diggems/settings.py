690f270b61299c8b04b1c3717270945467556fab
Tuanir FR <tfr011@gmail.com>
2013-07-20 09:17:23 -0300

    404 error

--- diggems/settings.py
+++ diggems/settings.py
@@ -19,7 +19,7 @@ FB_LIKE_URL = 'http://my.main.url/'
 EVENT_SERVER = '127.0.0.1:8080'
 
 # Django settings:
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -43,7 +43,6 @@ DATABASES = {
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
-
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
