39c64abf807c03c4431d42374b49c785815d1895
Lucas Clemente Vella <lvella@gmail.com>
2013-08-12 14:54:22 -0300

    Forcing TLS verification.

--- game/http_cli.py
+++ game/http_cli.py
@@ -9,6 +9,6 @@ def get_conn(base_url):
     try:
         return _pool[base_url]
     except KeyError:
-        cli = HTTPClient.from_url(base_url, connection_timeout=60, network_timeout=60, concurrency=5)
+        cli = HTTPClient.from_url(base_url, ssl_options=dict(ssl_version=ssl.PROTOCOL_TLSv1), connection_timeout=60, network_timeout=60, concurrency=5)
         _pool[base_url] = cli
         return cli
