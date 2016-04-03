916bb8d5dac88bc8536aabee4999146ee7e4de8e
Lucas Clemente Vella <lvella@gmail.com>
2016-04-03 04:21:41 -0300

    New database with pooling using guv.
    Thanks to the new Django database backend implementation, I could just
    subclass Postgres backend instead of copy-pasting it like the previous
    version.

---
+++ diggems/green_pool_psql/base.py
@@ -0,0 +1,59 @@
+from guv.semaphore import Semaphore
+from django.db.backends.postgresql.base import DatabaseWrapper as OrigDW
+from diggems.settings import DB_POOL_MAX_CONN
+
+import psycopg2.pool
+
+# A green connection pool
+class GreenConnectionPool(psycopg2.pool.AbstractConnectionPool):
+    def __init__(self, minconn, maxconn, *args, **kwargs):
+        self.semaphore = Semaphore(maxconn)
+        psycopg2.pool.AbstractConnectionPool.__init__(self, minconn, maxconn, *args, **kwargs)
+
+    def getconn(self):
+        self.semaphore.acquire()
+        conn = self._getconn()
+
+        # Turns close into putconn
+        conn.orig_close = conn.close
+        conn.close = lambda : self.putconn(conn)
+
+    def putconn(self, conn):
+        # Revert close to default behavior
+        conn.close = conn.orig_close
+
+        self._putconn(conn)
+        self.semaphore.release()
+
+    # Not sure what to do about this one...
+    closeall = psycopg2.pool.AbstractConnectionPool._closeall
+
+pools = {}
+
+class DatabaseWrapper(OrigDW):
+    def get_new_connection(self, conn_params):
+        try:
+            connection = self.pool.getconn()
+        except AttributeError:
+            global pools
+            try:
+                self.pool = pools[self.alias]
+            except KeyError:
+                self.pool = GreenConnectionPool(1, DB_POOL_MAX_CONN, **conn_params)
+                pools[self.alias] = self.pool
+            connection = self.pool.getconn()
+
+        # Unfortunatelly, the following had to be copy-pasted
+        # from Django's implementation
+        options = self.settings_dict['OPTIONS']
+        try:
+            self.isolation_level = options['isolation_level']
+        except KeyError:
+            self.isolation_level = connection.isolation_level
+        else:
+            # Set the isolation level to the value from OPTIONS.
+            if self.isolation_level != connection.isolation_level:
+                connection.set_session(isolation_level=self.isolation_level)
+
+        return connection
+
